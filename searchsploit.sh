cfdbee4a7f19b5a77b6c31ed9bea1b3cf306b697
Diego Rodríguez <diego25598@gmail.com>
2024-05-05 16:27:16 +0200

    add colors in messages

--- searchsploit.sh
+++ searchsploit.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+# Setting up colors for better readability
+
+greenColour="\e[0;32m\033[1m"
+endColour="\033[0m\e[0m"
+redColour="\e[0;31m\033[1m"
+blueColour="\e[0;34m\033[1m"
+yellowColour="\e[0;33m\033[1m"
+purpleColour="\e[0;35m\033[1m"
+turquoiseColour="\e[0;36m\033[1m"
+grayColour="\e[0;37m\033[1m"
+
 # Install dependencies if required
 
 if ! command -v git &> /dev/null || ! command -v searchsploit &> /dev/null; then
 
-    echo "Installing required dependencies"
+    echo -e "${turquoiseColour}Updating repositories${endColour}"
     sudo apt update > /dev/null
 
 fi
@@ -13,12 +24,12 @@ fi
 
 if ! command -v git &> /dev/null; then
 
-    echo "Installing Git"
+    echo -e "${turquoiseColour}Installing Git${endColour}"
     sudo apt install git -y > /dev/null
 
 else
 
-    echo "Git is already installed on the system"
+    echo -e "${greenColour}Git is already installed on the system${endColour}"
 
 fi
 
@@ -26,14 +37,16 @@ fi
 
 if ! command -v searchsploit &> /dev/null; then
 
-    echo "Installing searchsploit"
+    echo -e "${turquoiseColour}Installing searchsploit${endColour}"
     git clone https://gitlab.com/exploit-database/exploitdb.git && cd exploitdb/ && mv * /usr/bin && cd .. && rm -r exploitdb/
 
 else
 
-    echo "Searchsploit is already installed on the system"
+    echo -e "${greenColour}Searchsploit is already installed on the system${endColour}"
 
 fi
+
+echo -e "${greenColour}Dependencies installed${endColour}"
 
 echo "Dependencies installed"
 
